a9f58c65ee1eb51e7bfc2c3a6497b3cc54e2193c
grimmjow <chris.o.henning@gmail.com>
2010-04-07 20:26:01 +0200

    add: common classes to a common.py

---
+++ src/common.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python
+#coding=ut
+
+import time
+import re
+
+class Action:
+    """ Ausführen von/ direkte weitergabe an die devices """
+    # dev:command=1
+    def __init__(self, args, final):
+        args = args.split("=", 2)
+        self.value = args[1]
+        args = args[0].split(":", 2)
+        self.device_id = args[0]
+        self.command = args[1]
+        self.final = (final == "true")
+
+    def execute(self, states):
+        if states.devices.has_key(self.device_id):
+            states.devices[self.device_id].write(self.command + "=" + self.value)
+        return not self.final
+
+class Assignment:
+    """
+    Representiert die aktuelle Aufgabe.
+    Jede Aufgabe hat Ihre eigenen Events. die in der Hauptschleife Client.run()
+    verarbeitet werden.
+    Ein Assignment beginnt in der Regel mit einem Event (start_event) ohne
+    Bedingung und löst durch z.B. Bewegung weitere Events aus.
+    Das Assignment wird durch ein Event beendet.
+    Abschließend wird, wenn belegt, stop_event verarbeitet.
+    """
+
+    def __init__(self, id, startAction, stopAction):
+        self.id = id
+        self.active = False
+        self.startAction = startAction
+        self.stopAction = stopAction
+        self.events = []
+        self.lastprocessing = 0
+        self.starttime = None
+
+    def start(self, states):
+        """ aktiviert das Assignment """
+        self.active = True
+        self.starttime = time.time()
+        if self.startAction <> None:
+            self.startAction.execute(states)
+
+    def process(self, states):
+        """ verarbeitet neue Events """
+        if not self.active:
+            return 0
+
+        states.update("running", time.time() - self.starttime)
+        goon = (len(self.events) > 0)
+
+        for event in self.events:
+            goon = goon and event.check(states)
+
+        self.active = goon
+        return goon
+
+    def stop(self, states):
+        """ deaktiviert das Assignment """
+        if self.stopAction <> None:
+            self.stopAction.execute(states)
+        self.active = False
+
+class Argument:
+    """
+    Ist ein statischer Wert oder eine Abfrage an State.
+    """
+    ARG_STATIC = 1
+    ARG_STATE = 2
+
+    def __init__(self, arg):
+        # nur float (0.0)
+        if re.match("^([\d]+\.?[\d]*|[\d]*\.?[\d]+)$", arg, 0):
+            arg_typ = self.ARG_STATIC
+            arg_key = float(arg) / 100
+        else:
+            arg_typ = self.ARG_STATE
+            arg_key = arg
+        self.key = arg_key
+        self.typ = arg_typ
+
+    def get(self, states):
+        ret_val = None
+        if self.typ == self.ARG_STATIC:
+            ret_val = self.key
+        elif self.typ == self.ARG_STATE:
+            ret_val = states.getValue(self.key)
+        return ret_val
+
+class Event:
+    """
+    Events stellen Bedingungen und können den Client zu Bewegungen veranlassen.
+    """
+    def __init__(self, arg1, arg2, compare, action):
+        self.arg1 = arg1
+        self.arg2 = arg2
+        self.compare = compare
+        self.action = action
+
+    def check(self, states):
+        match = 0
+        goon = 1
+
+        if "g" in self.compare:
+            arg1 = self.arg1.get(states)
+            arg2 = self.arg2.get(states)
+            match = match or arg1 > arg2
+        if "l" in self.compare:
+            arg1 = self.arg1.get(states)
+            arg2 = self.arg2.get(states)
+            match = match or arg1 < arg2
+        if "e" in self.compare:
+            arg1 = self.arg1.get(states)
+            arg2 = self.arg2.get(states)
+            match = match or arg1 == arg2
+
+        if match:
+            goon = self.action.execute(states)
+        return goon
