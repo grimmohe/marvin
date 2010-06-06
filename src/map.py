adf9addb0b0acc49e16e20823751dcda086501d6
grimmjow <chris.o.henning@gmail.com>
2010-06-06 23:05:41 +0200

    map: add: modify skeleton and start merge method

--- src/map.py
+++ src/map.py
@@ -1,54 +1,65 @@
 #!/usr/bin/env python
 #coding=utf8
 
-class Point:
-    
-    def __init__(self):
-        self.map=None
-        self.x=0
-        self.y=0
-        
-class Line:
-    
-    def __init__(self):
-        self.begin=None
-        self.end=None
-        
-    def connectLine(self, l):
-        """ concenates single line """
-        pass
+""" global statics """
+MERGE_RANGE = 10
 
-    def connectLineList(self, ll):
-        """ concenates a list of lines together and to thisline """
-        pass
-        
+class Vector:
+    """
+    point (X;Y) with size (X;Y)
+    """
+    def __init__(self, x, y, size_x, size_y):
+        self.x = x
+        self.y = y
+        self.size_x = size_x
+        self.size_y = size_y
+
+    def startPoint(self):
+        return (self.x, self.y)
+
+    def endPoint(self):
+        return (self.x + self.size_x, self.y + self.size_y)
+
 class Area:
-    
-    def __init__(self):
-        self.lines=None
+    """
+    Closed ring of vectors
+    """
 
-    def mergeArea(self, a):
-        """ merge 2 areas together """
-        pass
-        
-class Map:
-    
     def __init__(self):
-        self.areas=None
-        self.lines=None
-        self.points=None
-        self.rasterSize=10
-        self.extrapolateTreshold=2
-        
-    def addPoint(self, p):
+        self.vectors = None
+        self.finished = False
+
+    def hasVactor(self, v):
+        """ check availability of vector v in self.vectors """
+        return (v in self.vectors)
+
+class Map:
+
+    def __init__(self):
+        self.areas = None
+        self.vectors = []
+        self.extrapolateThreshold = 2
+
+    def addVector(self, v):
         """ adding/merging a point to the map """
-        pass
-    
-    def addLine(self, l):
-        """ adding/merging a line to the map """ 
-        pass
-    
+        if v and v.__class__.__name__ == "Vector":
+            self.vectors.add(v)
+        else:
+            raise "Object None or wrong type. Expected Vector()"
+
     def addArea(self, a):
         """ adding/merging an area to the map """
         pass
 
+    def merge(self):
+        """ merge vectors that could be one """
+        ii = 0
+        aa = 0
+        max = len(self.vectors)
+        while ii < max:
+            v1 = self.vectors[ii]
+            while aa < max:
+                if ii == aa:
+                    continue
+                v2 = self.vectors[aa]
+                #TODO: jetzt prüfen, ob die nicht vielleicht zusammengeführt werden können
