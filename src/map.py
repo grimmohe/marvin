54026d03818c68d8904fc113354e700aeccea94f
grimmjow <chris.o.henning@gmail.com>
2010-08-10 15:10:37 +0200

    map: fix: always float for later calculations

--- src/map.py
+++ src/map.py
@@ -12,8 +12,8 @@ MAX_VECTOR_LENGTH = 50.0
 class Point:
 
     def __init__(self, x, y):
-        self.x = x
-        self.y = y
+        self.x = float(x)
+        self.y = float(y)
 
     def getDistanceTo(self, p2):
         return math.sqrt(math.pow(self.x - p2.x, 2) + math.pow(self.y - p2.y, 2))
