88e69d93b2028ed1f37a04b3f3e62987743f50a1
grimmjow <chris.o.henning@gmail.com>
2010-08-11 22:13:24 +0200

    unittest: add: Tests for map.Vector.copy() and combine()

--- src/tests/maptests.py
+++ src/tests/maptests.py
@@ -55,5 +55,58 @@ class TestAreaIntersections(unittest.TestCase):
         self.failIf(area.intersects(vector))
 
 
+class TestVectorCopy(unittest.TestCase):
+
+    def testVectorCopyEqual(self):
+        vector = map.Vector(map.Point(0, 2), map.Point(1, 0))
+        copy = vector.copy()
+        self.failUnless(vector.point.x == copy.point.x
+                        and vector.point.y == copy.point.y
+                        and vector.size.x == copy.size.x
+                        and vector.size.y == copy.size.y, copy)
+
+    def testVectorCopyMoved(self):
+        vector = map.Vector(map.Point(0, 2), map.Point(1, 0))
+        copy = vector.copy(map.Point(1, 1))
+        self.failUnless(copy.point.x == 1
+                        and copy.point.y == 3
+                        and copy.size.x == vector.size.x
+                        and copy.size.y == vector.size.y, copy)
+
+    def testVectorCopyTurned(self):
+        vector = map.Vector(map.Point(0, 2), map.Point(1, 0))
+        copy = vector.copy(map.Point(0, 0), 90)
+        self.failUnless(copy.point.x == 2
+                        and copy.point.y == 0
+                        and copy.size.x == 0
+                        and copy.size.y == -1, copy)
+
+    def testVectorCopyMovedAndTurned(self):
+        vector = map.Vector(map.Point(0, 2), map.Point(1, 0))
+        copy = vector.copy(map.Point(1, 1), 90)
+        self.failUnless(copy.point.x == 3
+                        and copy.point.y == 1
+                        and copy.size.x == 0
+                        and copy.size.y == -1, copy)
+
+
+class TestVectorCombine(unittest.TestCase):
+
+    def testVectorCombine(self):
+        vector1 = map.Vector(map.Point(0, 0), map.Point(5, 1))
+        vector2 = map.Vector(map.Point(0, 2), map.Point(5, 0))
+        combination = map.Vector().combine(vector1, vector2, map.Vector.START_POINT)
+        self.failUnless(combination.point.x == 0
+                        and combination.point.y == 0
+                        and combination.size.x == 0
+                        and combination.size.y == 2, combination)
+
+        combination = map.Vector().combine(vector1, vector2, map.Vector.END_POINT)
+        self.failUnless(combination.point.x == 5
+                        and combination.point.y == 1
+                        and combination.size.x == 0
+                        and combination.size.y == 1, combination)
+
+
 if __name__ == "__main__":
     unittest.main()
