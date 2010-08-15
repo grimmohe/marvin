51ffc1d6c13f9965d389e0165ef37e7058bd68b4
grimmjow <chris.o.henning@gmail.com>
2010-08-15 17:36:07 +0200

    unittest: add: map.Vector.copy()

--- src/tests/maptests.py
+++ src/tests/maptests.py
@@ -54,6 +54,48 @@ class TestAreaIntersections(unittest.TestCase):
         vector = map.Vector(map.Point(1, -3), map.Point(7, -1))
         self.failIf(area.intersects(vector))
 
+class TestVectorCopy(unittest.TestCase):
+
+    def testCopyNone(self):
+        vector = map.Vector(map.Point(1, 2), map.Point(3, 4))
+        copy = vector.copy()
+        self.failUnless(vector.point.x == copy.point.x)
+        self.failUnless(vector.point.y == copy.point.y)
+        self.failUnless(vector.size.x == copy.size.x)
+        self.failUnless(vector.size.y == copy.size.y)
+
+    def testCopyMove(self):
+        vector = map.Vector(map.Point(1, 2), map.Point(3, 4))
+        copy = vector.copy(map.Point(5, 2))
+        self.failUnless(copy.point.x == 6)
+        self.failUnless(copy.point.y == 4)
+        self.failUnless(copy.size.x == vector.size.x)
+        self.failUnless(copy.size.y == vector.size.y)
+
+    def testCopyMoveTurn(self):
+        vector = map.Vector(map.Point(1, 2), map.Point(3, 4))
+        copy = vector.copy(map.Point(5, 2), 90)
+        self.failUnless(copy.point.x == 7)
+        self.failUnless(copy.point.y == 1)
+        self.failUnless(copy.size.x == 4)
+        self.failUnless(copy.size.y == -3)
+
+    def testCopyOffset(self):
+        vector = map.Vector(map.Point(1, 2), map.Point(3, 0))
+        copy = vector.copy(offset=map.Vector(map.Point(0, 0), map.Point(0, 1)))
+        self.failUnless(copy.point.x == 1)
+        self.failUnless(copy.point.y == 3)
+        self.failUnless(copy.size.x == vector.size.x)
+        self.failUnless(copy.size.y == vector.size.y)
+
+    def testCopyAll(self):
+        vector = map.Vector(map.Point(1, 2), map.Point(4, 4))
+        copy = vector.copy(map.Point(5, 2), 90, map.Vector(map.Point(0, 0), map.Point(1, -1)))
+        self.failUnless(copy.point.x == 6)
+        self.failUnless(copy.point.y == 0)
+        self.failUnless(copy.size.x == 4)
+        self.failUnless(copy.size.y == -4)
+
 
 if __name__ == "__main__":
     unittest.main()
