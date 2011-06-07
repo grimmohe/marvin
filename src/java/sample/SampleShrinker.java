58559c7366816023334c488dfbbb8e50b262cfaf
grimm <grimm@c-base.org>
2011-06-07 22:52:34 +0200

    fix: shrinker out of bound exceptions

--- src/java/sample/SampleShrinker.java
+++ src/java/sample/SampleShrinker.java
@@ -12,18 +12,18 @@ public class SampleShrinker {
 		super();
 		this.sampleList = sampleList;
 	}
-	
+
 	public void shrink() {
-		
+
 		List<Sample> sampleListCopy = new ArrayList<Sample>();
 		sampleListCopy.addAll(sampleList);
-		
+
 		float previousDiff=0;
 		Sample previous=null;
 		for (Sample sample : sampleListCopy) {
 			if(previous != null) {
 				float diff = sample.getRow() - previous.getRow();
-				if(previousDiff != diff) {
+				if(previousDiff != diff && (diff > 1 || diff < 1) ) {
 					previousDiff = diff;
 				} else {
 					sampleList.remove(previous);
@@ -31,15 +31,16 @@ public class SampleShrinker {
 			}
 			previous = sample;
 		}
-		
-		// remove fist and last item
-		sampleList.remove(sampleList.get(0));
-		sampleList.remove(sampleList.get(sampleList.size()-1));
 
+		// remove fist and last item
+		if(sampleList.size()>2) {
+			sampleList.remove(sampleList.get(0));
+			sampleList.remove(sampleList.get(sampleList.size()-1));
+		}
 	}
-	
+
 	public List<Sample> getSampleList() {
 		return sampleList;
 	}
-	
+
 }
