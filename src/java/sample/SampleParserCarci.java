19212660adcd8f47a4538ce57727c7e30bf2c273
grimm <grimm@c-base.org>
2011-08-02 21:11:43 +0200

    _suppress_ compile error -_-

--- src/java/sample/SampleParserCarci.java
+++ src/java/sample/SampleParserCarci.java
@@ -12,9 +12,9 @@ public class SampleParserCarci implements SampleParser {
 	public SampleParserCarci(List<int[]> rgbList) {
 		super();
 	}
-	
+
 	public List<Sample> generateSamples(VideoFrame frame) {
-		
+
 		Raster raster = frame.getRaster();
 
 		int minY = raster.getMinY();
@@ -22,33 +22,34 @@ public class SampleParserCarci implements SampleParser {
 		int height = raster.getHeight();
 
 		for (int column = raster.getMinX(); column < width; column++) {
-			int[] rgbList = raster.getPixels(column, minY, 1, height, (int[]) null);		
+			int[] rgbList = raster.getPixels(column, minY, 1, height, (int[]) null);
 			int range = rgbList.length;
 			List<int[]> hotspots = findHotspots(rgbList);
-			
+
 			int pages = range/hotspots.size();
 		}
-		
+
 		return new ArrayList<Sample>();
-		
+
 	}
-	
+
 	private List<int[]> findHotspots(int[] rgbList) {
-		
-		List<int[]> hotspots = new ArrayList<int[]>();
-		int lastHeight=0;
-		for (int i=0; rgbList.length > i; i++) {
-			
-			int curr = rgb[i] + rgb[i+1] + rgb[i+2];
-			
-			if(curr > lastHeight) {
-				lastHeight = curr;
-			} else if (curr < lastHeight) {
-				hotspots.add(new int[] {i});
-			}
-		}
-		
-		return hotspots;
+
+//		List<int[]> hotspots = new ArrayList<int[]>();
+//		int lastHeight=0;
+//		for (int i=0; rgbList.length > i; i++) {
+//
+//			int curr = rgb[i] + rgb[i+1] + rgb[i+2];
+//
+//			if(curr > lastHeight) {
+//				lastHeight = curr;
+//			} else if (curr < lastHeight) {
+//				hotspots.add(new int[] {i});
+//			}
+//		}
+//
+//		return hotspots;
+	return null;
 	}
-	
+
 }
