7be3932cfff5413a7952146be70d6051585cd368
grimm <grimm@c-base.org>
2011-09-20 21:03:03 +0200

    conf: angle conf for cheep cam

--- src/java/conf/Configuration.java
+++ src/java/conf/Configuration.java
@@ -1,15 +1,13 @@
 package conf;
 
-import java.util.HashMap;
-import java.util.Map;
 
 public class Configuration {
 
 	public static final int videoFramesMin = 10;
-	public static final int videoVAngle = 43; //42,91
+	public static final int videoVAngle = 25; //42,91
 	public static final int videoHAngle = 26;
 	public static final int videoLaserDistance = 8;
-	public static final int videoViewVAngle = 43;
+	public static final int videoViewVAngle = 25;
 	public static final int logginPort = 2889;
 	public static final int noiseFrameCache = 3;
 
